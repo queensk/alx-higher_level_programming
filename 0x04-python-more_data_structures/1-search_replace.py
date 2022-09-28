5aeaf6b40d5d23cf7de42a7a26348505ccd84f1d
queensk <queenskisiculi@gmail.com>
2022-09-28 20:31:48 +0000

    fixed copying error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     i = 0
     for item in my_list:
         if item is search:
-            my_list[i] = replace
+            new_list[i] = replace
         i += 1
     return (new_list)
