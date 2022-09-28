b7acb4cd07aab9843bd9d9257560ebda3dd493cf
queensk <queenskisiculi@gmail.com>
2022-09-28 20:19:14 +0000

    fixed error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
+    new_list = my_list.copy()
     i = 0
     for item in my_list:
         if item is search:
-            mylist[i] = replace
-        i++
+            my_list[i] = replace
+        i += 1
     return (new_list)
