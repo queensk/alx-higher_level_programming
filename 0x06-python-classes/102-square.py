789b55732a4495ca3d517ed279dc24b873d62e0d
queens kisivuli <queenskisivuli@gmail.com>
2022-10-20 07:07:37 +0300

    fixed pycode style

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,19 +1,21 @@
-#usr/bin/python3
+#!/usr/bin/python3
 """
  Creates an empty class called Square
 """
+
 
 class Square:
     """
     Empty class with size private attribute
     """
+
     def __init__(self, size=0):
         """
                 Instantiation with size
         Args:
             size: size of the square
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
@@ -24,7 +26,7 @@ class Square:
         Returns the area of the square
         """
         return (self.__size * self.__size)
-        
+
     @property
     def size(self):
         """
@@ -35,46 +37,60 @@ class Square:
     @size.setter
     def size(self, value):
         """
-        size setter. Set the size square
+                size setter. Set the size square
+        Args:
+            value: value of size
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def __lt__(self, other):
-        """ 
-        Compare if our square is less than the other
+        """
+                Compare if our square is less than the other
+        Args:
+            other: Area of square to be compared
         """
         return self.area() < other.area()
 
     def __le__(self, other):
         """
         Compare if our square is less ot equal than the other
+        Args:
+            other: value of area of square to be compared
         """
         return self.area() <= other.area()
-        
+
     def __eq__(self, other):
         """
         Compare if our square is equal than the other
+        Args:
+            other: value of area of square to be compared
         """
         return self.area() == other.area()
-        
+
     def __ne__(self, other):
         """
         Compare if our square is different than the other
+        Args:
+            other: value of area of square to be compared
         """
         return self.area() != other.area()
-        
+
     def __gt__(self, other):
         """
         Compare if our square is greater than the other
+        Args:
+            other: value of area of square to be compared
         """
         return self.area() > other.area()
-        
+
     def __ge__(self, other):
         """
         Compare if our square is less than the other
+        Args:
+            other: value of area of square to be compared
         """
         return self.area() >= other.area()
