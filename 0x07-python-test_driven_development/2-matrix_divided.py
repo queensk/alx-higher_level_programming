a278553e2847c8d304bb3d43d704cefac18096dc
queens kisivuli <queenskisivuli@gmail.com>
2022-10-20 15:40:20 +0300

    function that divides all elements of a matrix.

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+This module is composed by a function that divides the numbers of a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Function that divides the integer/float numbers of a matrix
+    Args:
+        matrix: list of a lists of integers/floats
+        div: number which divides the matrix
+
+    Returns:
+        return a matrix with values from division
+    Raises:
+        TypeError: If the elements of the matrix aren't lists
+                   If the elements of the lists aren't integers/floats
+                   If div is not an integer/float number
+                   If the lists of the matrix don't have the same size
+
+        ZeroDivisionError: If div is zero
+    """
+    if not isinstance(div, [int, float]):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(msg_type)
+
+    len_e = 0
+    msg_error = "Each row of the matrix must have the same size"
+
+    for items in matrix:
+        if not items or not isinstance(items, list):
+            raise TypeError(msg_error)
+
+        if len_e != 0 and len(items) != len_e:
+            raise TypeError(msg_error)
+
+        for num in items:
+            if not isinstance(num, (int, float)):
+                raise TypeError(msg_error)
+
+        len_e = len(items)
+
+    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (m)
