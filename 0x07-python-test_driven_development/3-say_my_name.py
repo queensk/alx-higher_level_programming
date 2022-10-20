d981bf647e1048beea252093c59eca6bfee924bd
queens kisivuli <queenskisivuli@gmail.com>
2022-10-20 16:44:31 +0300

    fixed pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,6 +2,8 @@
 """
 This module contains a function that prints first and last name.
 """
+
+
 def say_my_name(first_name, last_name=""):
     """
     Function that prints My name is <first name> <last name>.
@@ -9,19 +11,19 @@ def say_my_name(first_name, last_name=""):
     Args:
         first_name: value of the first name.
         last_name: value of the last name.
-    
+
     Returns:
         No return value.
 
         TypeError: If first_name or last_name is not a string
 
-    
+
     """
 
     if not isinstance(first_name, str):
-        TypeError ("first_name must be a string")
-    
+        TypeError("first_name must be a string")
+
     if not isinstance(last_name, str):
-        TypeError ("last_name must be a string")
+        TypeError("last_name must be a string")
 
     print(f"My name is {first_name} {last_name}")
