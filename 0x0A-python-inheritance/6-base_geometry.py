dcdc425e1b78e930be7855bbe003903785221482
queens kisivuli <queenskisivuli@gmail.com>
2022-10-25 13:28:40 +0300

    raise exemption area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+5-base_geometry: class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeaometry
+    Methods:
+        area: raise and exception.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
