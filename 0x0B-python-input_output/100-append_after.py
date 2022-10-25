4c3e4c528d6dfa66ef58d57e562eb90729777098
queensk <queenskisivuli@gmail.com>
2022-10-25 19:24:45 +0000

    fixed file encoding

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -19,5 +19,5 @@ def append_after(filename="", search_string="", new_string=""):
             if line.find(search_string) != -1:
                 res_line += [new_string]
 
-    with open(filename, 'w', encoding="utf-6") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         f.write("".join(res_line))
