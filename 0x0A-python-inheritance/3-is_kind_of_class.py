61fcc8b7bf19722b8ec9deb6a092ee71aebf39fc
queens kisivuli <queenskisivuli@gmail.com>
2022-10-25 10:58:57 +0300

    object is an instance of a class and subclass

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function that returns True if the object is an instance of,
+or if the object is an instance of a class that inherited from,
+the specified class ; otherwise False.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks if object is instance of a class and its subclass.
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
