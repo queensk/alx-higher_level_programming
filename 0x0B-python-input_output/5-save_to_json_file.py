26856c117c2315d71a0ded1dadd580609bdff4c6
queens kisivuli <queenskisivuli@gmail.com>
2022-10-26 08:18:46 +0300

    save object to file test

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -16,4 +16,4 @@ def save_to_json_file(my_obj, filename):
         exemption when file cannot be append.
     """
     with open(filename, "w", encoding="utf-8") as f:
-        json.dumps(my_obj, f)
+        json.dump(my_obj, f)
