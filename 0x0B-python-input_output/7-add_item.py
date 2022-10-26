b087a94502359f4f32ac50a3a61a6a533805bb90
queens kisivuli <queenskisivuli@gmail.com>
2022-10-26 09:01:00 +0300

    fixed pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,15 +9,15 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == '__main__':
-    
+
     filename = "add_item.json"
-    
+
     try:
         arg_list = load_from_json_file(filename)
-    except:
+    except BaseException:
         arg_list = []
 
     for arg in sys.argv[1:]:
         arg_list.append(arg)
-    
+
     save_to_json_file(arg_list, filename)
