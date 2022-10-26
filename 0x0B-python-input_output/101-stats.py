de2260fc483ede2cff8a9a964d853b472f59367e
queens kisivuli <queenskisivuli@gmail.com>
2022-10-26 05:53:25 +0300

    added test file

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+A script that reads stdin line by line and computes metrics
+"""
 import sys
 
 
 def print_info():
+    """
+    Prints Total file size: File size: <total size>
+    """
     print('File size: {:d}'.format(file_size))
 
     for scode, code_times in sorted(status_codes.items()):
