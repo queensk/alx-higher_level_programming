f5283e83a78419f83ea9b467b22e1d528820a018
queens kisivuli <queenskisivuli@gmail.com>
2022-11-29 23:53:08 +0300

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- creates a table called first_table in current database
+-- database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS first_table(id INT,
+name VARCHAR(256));
