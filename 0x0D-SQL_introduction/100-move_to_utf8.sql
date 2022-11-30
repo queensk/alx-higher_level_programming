fc41bdd88fe46b0f20913432425f245b93eb21ca
queens kisivuli <queenskisivuli@gmail.com>
2022-11-30 11:16:46 +0300

    fixed error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,7 +8,6 @@ ALTER DATABASE hbtn_0c_0
 
 ALTER TABLE
       `first_table`
-      CHANGE `name` `name`
-      VARCHAR(256)
+      CONVERT TO
       CHARACTER SET utf8mb4
       COLLATE utf8mb4_unicode_ci;
