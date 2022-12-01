6f70cd925eb61c7f433c3b5a85bd00acde5710f7
queens kisivuli <queenskisivuli@gmail.com>
2022-12-01 23:57:41 +0300

    Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,16 @@
+-- creates the database hbtn_0d_usa 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- creates table cities: cities description:
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) can’t be null
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id)
+		REFERENCES hbtn_0d_usa.states(id)
+);
