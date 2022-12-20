804e25022e30475388f847f5c14f3d6f1453f5b1
queensk <queenskisivuli@gmail.com>
2022-12-20 22:05:13 +0300

    prints square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// Prints Square
+const size = process.argv[2];
+
+if (/^-?\d+$/.test(size)) {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
