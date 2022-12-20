90948d2c6ef901c53f1e68b99d7f21e9f6e46dae
queensk <queenskisivuli@gmail.com>
2022-12-20 21:28:37 +0300

    prints using loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// prints 3 lines: (like 1-multi_languages.js),
+// but by using an array of string and a loop
+
+const multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < multiLanguages.length; i++) {
+  console.log(multiLanguages[i]);
+}
