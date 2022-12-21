1f4413c1f2b1528013e9c2e6cc484c73930c168b
queensk <queenskisivuli@gmail.com>
2022-12-21 05:57:02 +0300

    fixed concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -10,6 +10,6 @@ const destination = process.argv[4];
 // read content of the files
 const dataFileOne = fs.readFileSync(filePathOne, 'utf8');
 const dataFileTwo = fs.readFileSync(filePathTwo, 'utf8');
-const concatData = [dataFileOne, dataFileTwo].join('\n');
+const concatData = dataFileOne + dataFileTwo;
 
 fs.writeFileSync(destination, concatData);
