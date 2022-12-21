e42f8a788184f6c64b9d56b023beacf72289b7b3
queensk <queenskisivuli@gmail.com>
2022-12-21 22:31:09 +0300

    export addMeMaybe

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// function that returns the number of occurrences in a list:
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
