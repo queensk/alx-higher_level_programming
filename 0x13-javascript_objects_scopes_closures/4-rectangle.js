1727523e5e33382acdf0b7f8c8adf7fb6abca766
queensk <queenskisivuli@gmail.com>
2022-12-21 21:30:33 +0300

    fixed style error

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -23,12 +23,12 @@ module.exports = class Rectangle {
   }
 
   // Rotates the Rectangle
-  rotate(){
+  rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
 
   // Double the value of the width and height
-  double(){
-    [this.width, this.height] = [this.width*2, this.height*2];
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
 };
