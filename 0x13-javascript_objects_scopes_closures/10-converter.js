5aebc3ad63574e71f2573bfaf4ac43a86a928d27
queensk <queenskisivuli@gmail.com>
2022-12-21 23:06:51 +0300

    fixed style error

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,8 +2,7 @@
 
 // function that converts a number from base 10 to another base passed as argument:
 exports.converter = function (base) {
-    return function (num) {
-      return num.toString(base);
-    }
-  }
-  
+  return function (num) {
+    return num.toString(base);
+  };
+};
