8b4d6dd8eeab435adc115066d3b9a79bbb8c34b7
queensk <queenskisivuli@gmail.com>
2022-12-21 22:36:53 +0300

    fixed style error

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-//function that returns the reversed version of a list:
+// function that returns the reversed version of a list:
 
-exports.esrever = function(list) {
-    const reversedList = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-      reversedList.push(list[i]);
-    }
-    return reversedList;
+exports.esrever = function (list) {
+  const reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
   }
-  
+  return reversedList;
+};
