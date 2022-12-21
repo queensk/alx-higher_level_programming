c052c7203c57b761964c718290f734854b20b303
queensk <queenskisivuli@gmail.com>
2022-12-21 17:48:52 +0300

    fixed return type

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,12 +3,11 @@
 // class Rectangle that defines a rectangle:
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w < 0 || h < 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      this.width = 0;
-      this.hight = 0;
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return this.Rectangle;
     } else {
       this.width = w;
-      this.hight = h;
+      this.height = h;
     }
   }
 };
