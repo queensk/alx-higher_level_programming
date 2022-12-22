7e55586bcc22679fa933ecbf7d564b02bb3043a1
queensk <queenskisivuli@gmail.com>
2022-12-22 17:14:24 +0300

    code refactoring

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-lists all states from the database hbtn_0e_0_usa
+script that takes in an argument and,
+displays all values in the states table,
+of hbtn_0e_0_usa where name matches the argument.
 """
 
 from sys import argv
@@ -10,19 +12,13 @@ if __name__ == '__main__':
     """
     access the database and update state
     """
-
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states \
-            WHERE name = "{}" \
-            ORDER BY id ASC'.format(argv[4]))
+                    WHERE name = "{}" \
+                    ORDER BY id ASC'.format(argv[4]))
 
     for row in cursor.fetchall():
         print(row)
