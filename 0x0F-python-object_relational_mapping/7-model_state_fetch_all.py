1c36038ac6a7bcd550298007e804dbf5e15e5311
queensk <queenskisivuli@gmail.com>
2022-12-22 23:30:43 +0300

    updated models

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,17 +7,16 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     connect to the database and transect with state table
     """
-    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]
-    )
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
 
-    engine = create_engine(db_url)
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
 
     session = Session()
     for instance in session.query(State).order_by(State.id):
-        print('{}: {}'.format(instance.id, instance.name))
+        print('{0}: {1}'.format(instance.id, instance.name))
