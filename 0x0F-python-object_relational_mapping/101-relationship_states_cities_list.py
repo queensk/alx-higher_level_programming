eac8ce771ff1e422d0a2365f402149fede09055a
queensk <queenskisivuli@gmail.com>
2022-12-23 09:06:27 +0300

    List relationship

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker
+from relationship_state import State, Base
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                        format(argv[1], argv[2], argv[3]),
+                        pool_pre_ping=True)
+    Base.metadata.crate_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_city = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+
+    for state in state_city:
+        print("{}: {}".format(state.id, state.name))
+        for city in state_city:
+            print("    {}: {}".format(city.id, city.name))
+
+    session.close()
+    
