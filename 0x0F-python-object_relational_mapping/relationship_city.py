a669aafb038dc733f30e96fe04d9e8d3f6b0b716
queensk <queenskisivuli@gmail.com>
2022-12-23 08:34:44 +0300

    fixed relationship import

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,7 +4,7 @@ contains the class definition of a,
 State and an instance Base = declarative_base():
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from relationship_state import Base, State
 
 
 class City(Base):
