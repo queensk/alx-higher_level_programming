ff0ee6bfe5f1cc5bb6f8bf236a962c6e25dc19d6
queensk <queenskisivuli@gmail.com>
2022-12-23 08:30:57 +0300

    fixed state

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -22,4 +22,4 @@ class State(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+    cities = relationship("City", backref="state", cascade="all, delete")
