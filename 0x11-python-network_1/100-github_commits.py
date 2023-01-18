58970ba53db67de683aff8665164efffecb7aa37
queensk <queenskisivuli@gmail.com>
2023-01-18 06:00:29 +0300

    update pycode style

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Lists the 10 most recent commits on a given GitHub repository.
+"""
+
+import requests
+import sys
+if __name__ == "__name__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[1], sys.argv[2]
+    )
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
