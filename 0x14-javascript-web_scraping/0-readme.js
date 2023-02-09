e21a9f14ba2b3b5841b51c0288602f1f43916e3a
queensk <queenskisivuli@gmail.com>
2023-02-09 21:05:06 +0300

    fixed semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 // script that reads and prints the content of a file.
-const fs = require("fs");
+const fs = require('fs');
 const file = process.argv[2];
-fs.readFile(file, "utf-8", (err, data) => {
+
+fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
